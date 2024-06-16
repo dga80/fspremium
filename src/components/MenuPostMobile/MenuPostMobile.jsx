3d2ab526f0d3eb146ff96caabd288e59feca0acf
dga80 <dani.dga@gmail.com>
2024-06-16 15:31:43 +0200

    Latest & Popular mobile

--- src/components/MenuPostMobile/MenuPostMobile.jsx
+++ src/components/MenuPostMobile/MenuPostMobile.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import './MenuPostMobile.css'
 import { ReactComponent as FilterMobile } from '../../elements/FilterMobile.svg'
 
-const MenuPost = () => {
+const MenuPost = ({ showLatest, showPopular, isPopular }) => {
     return (
-
         <div className='menuPost'>
             <div className='menuButtons1'>
-                <div className='latest'>Latest</div>
-                <div className='popular'>Popular</div>
+                <div className={`latest ${!isPopular ? 'active' : ''}`} onClick={showLatest}>Latest</div>
+                <div className={`popular ${isPopular ? 'active' : ''}`} onClick={showPopular}>Popular</div>
             </div>
             <div className='menuButtons2'>
                 <div className='filterMobile'>
